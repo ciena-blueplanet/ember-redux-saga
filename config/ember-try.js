f0b51abcab20040e6a0fd2cf9a51e907bd4f269e
toran billups <toranb@gmail.com>
2017-04-30 20:33:01 -0500

    [BUILD]: pulled beta build from travis config

--- config/ember-try.js
+++ config/ember-try.js
@@ -49,28 +49,6 @@ module.exports = {
           'ember': '~2.10.0'
         }
       }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
     }
   ]
 }
